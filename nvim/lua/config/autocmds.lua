aaa97d0193288a4eb3ce2159d547a77b8d24a935
ariankoochak <arian.koochak@protonmail.com>
2026-09-19 13:05:57 +0330

    add open default terminal

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -37,3 +37,32 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end,
 })
+
+local terminal_opened = false
+
+-- Open one bottom terminal when the session first enters a real file buffer.
+local function open_terminal_for_file(buf)
+  if terminal_opened or vim.bo[buf].buftype ~= "" or vim.api.nvim_buf_get_name(buf) == "" then
+    return
+  end
+
+  terminal_opened = true
+  local editor_win = vim.api.nvim_get_current_win()
+  vim.schedule(function()
+    Snacks.terminal.open(nil, {
+      interactive = false,
+      win = { position = "bottom", height = 10 },
+    })
+    if vim.api.nvim_win_is_valid(editor_win) then
+      vim.api.nvim_set_current_win(editor_win)
+    end
+  end)
+end
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  group = augroup("terminal_on_first_file"),
+  callback = function(event) open_terminal_for_file(event.buf) end,
+})
+
+-- LazyVim can load this file after an initial command-line file is already open.
+vim.schedule(function() open_terminal_for_file(vim.api.nvim_get_current_buf()) end)
